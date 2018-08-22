cc6649c9b9b4a457e5f245aa1225b58a9ae1ef64
SUMEDHA <sumedhathakur98@gmail.com>
2018-08-22 10:49:14 +0530

    assignment1

---
+++ java.java
@@ -0,0 +1,21 @@
+------------------------------------------GIT-------------------------------------
+
+GIT : It is a Version Control System.
+
+TERMINOLOGY : 
+
+
+1. Repository
+
+2. Push And Pull 
+
+
+GIT terms : 
+
+git config --global username
+
+git config --global username.email
+
+git init
+
+git status
